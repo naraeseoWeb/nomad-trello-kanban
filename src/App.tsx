e0437bded9437da9cfc964eaa718d37ffb51f55a
naraeseo <naraeseoez@gmail.com>
2022-11-01 11:53:00 +0900

    칸반보드 라이브러리를 사용해 기본 형태 잡기

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,68 @@
+import { DragDropContext, Draggable, Droppable } from '@hello-pangea/dnd';
 import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  display: flex;
+  width: 100%;
+  margin: 0 auto;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const Boards = styled.div`
+  display: grid;
+  width: 100%;
+  grid-template-columns: repeat(1, fr);
+`;
+
+const Board = styled.div`
+  padding: 20px 10px;
+  background-color: ${(props) => props.theme.boardColor};
+  border-radius: 5px;
+  min-height: 200px;
+`;
+
+const Card = styled.div`
+  background-color: ${(props) => props.theme.cardColor};
+  padding: 10px 10px;
+  border-radius: 5px;
+  margin-bottom: 5px;
+`;
+
+const toDos = ['a', 'b', 'c', 'd', 'e', 'f'];
 
 function App() {
+  const onDragEnd = () => {};
+
   return (
-    <div className='App'>
-      <header className='App-header'>dfdffdfdf</header>
-    </div>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Wrapper>
+        <Boards>
+          <Droppable droppableId='one'>
+            {(magic, info) => (
+              <Board ref={magic.innerRef} {...magic.droppableProps}>
+                {toDos.map((toDo, index) => (
+                  <Draggable draggableId={toDo} index={index}>
+                    {(magic) => (
+                      <Card
+                        {...magic.dragHandleProps}
+                        {...magic.draggableProps}
+                        ref={magic.innerRef}
+                      >
+                        {toDo}
+                      </Card>
+                    )}
+                  </Draggable>
+                ))}
+                {magic.placeholder}
+              </Board>
+            )}
+          </Droppable>
+        </Boards>
+      </Wrapper>
+    </DragDropContext>
   );
 }
 
