ac392b1679e29b5766cd9bd5e4ee731b4beced0d
naraeseo <naraeseoez@gmail.com>
2022-11-03 15:35:18 +0900

    multiboard 분리, 에러발생

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ import Board from './Components/Board';
 const Wrapper = styled.div`
   display: flex;
   max-width: 680px;
-  width: 100%;
   margin: 0 auto;
   justify-content: center;
   align-items: center;
@@ -21,18 +20,39 @@ const Boards = styled.div`
   align-items: flex-start;
   width: 100%;
   gap: 10px;
+  border-radius: 5px;
 `;
 
 const App = () => {
   const [toDos, setToDos] = useRecoilState(toDoState);
-  const onDragEnd = ({ draggableId, destination, source }: DropResult) => {
+  const onDragEnd = (info: DropResult) => {
+    const { destination, draggableId, source } = info;
     if (!destination) return;
-    // setToDos((oldToDos) => {
-    //   const toDosCopy = [...oldToDos];
-    //   toDosCopy.splice(source.index, 1);
-    //   toDosCopy.splice(destination?.index, 0, draggableId);
-    //   return toDosCopy;
-    // });
+    if (destination?.droppableId === source.droppableId) {
+      setToDos((allBoards) => {
+        const boardCopy = [...allBoards[source.droppableId]];
+        const taskObj = boardCopy[source.index];
+        boardCopy.splice(source.index, 1);
+        boardCopy.splice(destination?.index, 0, taskObj);
+        return {
+          ...allBoards,
+          [source.droppableId]: boardCopy,
+        };
+      });
+    }
+    if (destination?.droppableId !== source.droppableId) {
+      setToDos((allBoards) => {
+        const sourceBoard = [...allBoards[source.droppableId]];
+        const destinationBoard = [...allBoards[destination.droppableId]];
+        sourceBoard.splice(source.index, 1);
+        destinationBoard.splice(destination?.index, 0, draggableId);
+        return {
+          ...allBoards,
+          [source.droppableId]: sourceBoard,
+          [destination.droppableId]: destinationBoard,
+        };
+      });
+    }
   };
   console.log(toDos, 'toDos');
 
--- src/Components/Board.tsx
+++ src/Components/Board.tsx
@@ -3,10 +3,27 @@ import styled from 'styled-components';
 import DraggableCard from './DraggableCard';
 
 const Wrapper = styled.div`
-  padding: 20px 10px;
+  width: 300px;
+  padding-top: 10px;
   background-color: ${(props) => props.theme.boardColor};
   border-radius: 5px;
-  min-height: 200px;
+  min-height: 300px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Title = styled.h2`
+  text-align: center;
+  font-weight: 600;
+  margin-bottom: 10px;
+  font-size: 18px;
+`;
+
+const Area = styled.div`
+  background-color: #dfe6e9;
+  flex-grow: 1;
+  transition: all;
+  padding: 20px;
 `;
 
 interface IBoardProps {
@@ -16,16 +33,19 @@ interface IBoardProps {
 
 const Board = ({ toDos, boardId }: IBoardProps) => {
   return (
-    <Droppable droppableId='one'>
-      {(magic) => (
-        <Wrapper ref={magic.innerRef} {...magic.droppableProps}>
-          {toDos.map((toDo, index) => (
-            <DraggableCard key={toDo} toDo={toDo} index={index} />
-          ))}
-          {magic.placeholder}
-        </Wrapper>
-      )}
-    </Droppable>
+    <Wrapper>
+      <Title>{boardId}</Title>
+      <Droppable droppableId='one'>
+        {(magic) => (
+          <Area ref={magic.innerRef} {...magic.droppableProps}>
+            {toDos.map((toDo, index) => (
+              <DraggableCard key={toDo} toDo={toDo} index={index} />
+            ))}
+            {magic.placeholder}
+          </Area>
+        )}
+      </Droppable>
+    </Wrapper>
   );
 };
 
